ad09af78a29633502c60681fb0a06b36f8520651
S Stewart <hi@youneeda.pw>
2017-04-22 17:33:39 -0500

    [merge] ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const g = require('gulp')
 const cn = require('gulp-cssnano')
 const rs = require('run-sequence')
+const ap = require('gulp-autoprefixer')
 
 g.task('default', function() {
   return g.src('src/byd.css')
   .pipe(cn())
+  .pipe(ap())
   .pipe(g.dest('out'))
 })
 
