ae7702217e849b69ae0b421e8cc7abe6d5241c16
S Stewart <hi@youneeda.pw>
2017-05-31 18:09:05 -0500

    [gulpfile, test] improve gulp, fix test

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const g = require('gulp')
 const cn = require('gulp-cssnano')
-const rs = require('run-sequence')
 const ap = require('gulp-autoprefixer')
 
-g.task('default', function() {
+g.task('default', function () {
   return g.src('src/byd.css')
   .pipe(cn())
   .pipe(ap())
   .pipe(g.dest('out'))
 })
-
