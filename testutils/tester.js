281276ee4efb329011e3aa726cec161e6f4462a3
S Stewart <poweredby@linuxmail.org>
2017-06-03 10:29:35 -0500

    [tester, pakcage] bit of things

--- testutils/tester.js
+++ testutils/tester.js
@@ -12,7 +12,7 @@ const spinner = spin()
 const hello = 'Hello - welcome to the BYD test suite.'
 const hello2 = 'Please press Ctrl-C if you changed your mind or got here by mistake. We\'ll start in 3 seconds.'
 
-const msg = 'Testing gulpfile with standard...'
+const msg = 'Testing JS with standard...'
 const msg2 = 'Running gulp...'
 const msg3 = 'All done!'
 
@@ -26,21 +26,21 @@ async function test () {
   await sleep(1500)
   spinner.text = chalk.yellow(hello2)
   await sleep(3000)
-  spinner.text = msg
-  if (sh.exec('standard --fix gulpfile.js').code !== 0) {
-    spinner.fail('Gulpfile check failed - please raise a issue')
+  spinner.text = msg + '\n'
+  if (sh.exec('standard --fix').code !== 0) {
+    spinner.fail('Style check failed - please raise a issue')
     process.exit(1)
   } else {
-    spinner.succeed('Gulpfile check succeeded')
+    spinner.succeed('Style check succeeded')
   }
+  spinner.text = msg2 + '\n'
   spinner.start()
   if (sh.exec('gulp').code !== 0) {
     spinner.fail('Gulp failed for some reason... :/')
+    process.exit(1)
   } else {
     spinner.succeed('Gulp ran successfully.')
-    process.exit(1)
   }
-  spinner.start()
   spinner.succeed(chalk.green(msg3))
   process.exit(0)
 }
