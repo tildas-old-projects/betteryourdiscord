636688e6979a5c0ad4be9307dbd9a0073dcf8b6f
S Stewart <poweredby@linuxmail.org>
2017-06-03 10:18:50 -0500

    [gulpfile, tests, package] fix critical error, roll out new tests

--- testutils/tester.js
+++ testutils/tester.js
@@ -1,23 +1,48 @@
 // colors in chalk
-// and (fidget) spinners by "ora"
+// shell by shelljs
+// and (fidget) spinners by ora
 const chalk = require('chalk')
+const sh = require('shelljs')
 const spin = require('ora')
 
+// VSHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 const spinner = spin()
 
-let evalcmd
-
+// Messages for spinner/logging
 const hello = 'Hello - welcome to the BYD test suite.'
 const hello2 = 'Please press Ctrl-C if you changed your mind or got here by mistake. We\'ll start in 3 seconds.'
 
 const msg = 'Testing gulpfile with standard...'
-const msg2 = 'Checking exit code...'
-const msg3 = 'Running gulp...'
-const msg4 = 'All done!'
+const msg2 = 'Running gulp...'
+const msg3 = 'All done!'
 
-console.log(chalk.green(hello))
-spinner.start()
-setTimeout(() => {
-    spinner.text = chalk.yellow(hello2)
-}, 3000)
-spinner.text = msg
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function test () {
+  spinner.start()
+  spinner.text = chalk.green(hello)
+  await sleep(1500)
+  spinner.text = chalk.yellow(hello2)
+  await sleep(3000)
+  spinner.text = msg
+  if (sh.exec('standard --fix gulpfile.js').code !== 0) {
+    spinner.fail('Gulpfile check failed - please raise a issue')
+    process.exit(1)
+  } else {
+    spinner.succeed('Gulpfile check succeeded')
+  }
+  spinner.start()
+  if (sh.exec('gulp').code !== 0) {
+    spinner.fail('Gulp failed for some reason... :/')
+  } else {
+    spinner.succeed('Gulp ran successfully.')
+    process.exit(1)
+  }
+  spinner.start()
+  spinner.succeed(chalk.green(msg3))
+  process.exit(0)
+}
+
+test()
